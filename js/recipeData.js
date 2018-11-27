43a84e9ec8b3bc559e3f038ea45993788cdd8791
Christine Dai <macroavis@gmail.com>
2018-11-26 17:45:27 -0800

    replaced all links to inventory with inventoryNew

---
+++ js/recipeData.js
@@ -0,0 +1,10 @@
+// this stores the mock database we will be using
+let mockRecipeDatabase = [
+	{
+		id: -1,
+		name: 'sample',
+		tags: ['vegetarian', 'vegan'],
+		url: 'www.google.com',
+		image: 'www.google.com'
+	},
+];
